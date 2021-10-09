5c03d74c7a64d2c732f7d19748ee67072e2340e5
Chandra Lama <iamme24cl@gmail.com>
2021-10-09 15:00:44 -0700

    defined current user and current shopping cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
-class ApplicationController < ActionController::Base
-  
+class ApplicationController < ActionController::API
+  include ActionController::Cookies
+
+  def current_user
+    @current_user ||= User.find_by(id: cookies[:user_id] if cookies[:user_id])
+  end
+
+  def current_shopping_cart
+    @current_shopping_cart ||= ShopingCart.find_by(id: cookies[:shopping_cart_id] if cookies[:shopping_cart_id])
+  end
 end
