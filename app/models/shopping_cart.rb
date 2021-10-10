fd672a1f31f3b6f7aff4b8071a26cc3d6c12ce8d
Chandra Lama <iamme24cl@gmail.com>
2021-10-10 12:38:41 -0700

    added validation for status attribute on the shopping cart model

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,4 +1,6 @@
 class ShoppingCart < ApplicationRecord
   has_many :shopping_cart_items
   belongs_to :user, optional: true
+
+  validates :status, acceptance: { accept: ['completed', 'pending'] }
 end
