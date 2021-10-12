b6100ed0ccaf4363d95add87ec3ea73898359e56
Chandra Lama <iamme24cl@gmail.com>
2021-10-12 14:34:33 -0700

    added routes for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'login_status' => 'users#login_status'
+
+  resources :users, only: [:create, :update, :destroy]
   resources :sessions, only: [:create, :destroy]
 
 end
