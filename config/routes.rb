8ee56b277ddc754e522afc0d34ae09e9e0b12640
Chandra Lama <iamme24cl@gmail.com>
2021-10-13 13:57:57 -0700

    added index route for users for testing purposes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get 'login_status' => 'users#login_status'
 
-  resources :users, only: [:create, :update, :destroy]
+  resources :users, only: [:index, :create, :update, :destroy]
   resources :sessions, only: [:create, :destroy]
 
 end
