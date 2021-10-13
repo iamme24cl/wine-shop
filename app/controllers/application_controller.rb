71826d5540e5dd18f68b41e78647b26e248a7a57
Chandra Lama <iamme24cl@gmail.com>
2021-10-13 13:57:17 -0700

    debugged syntax error in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,10 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
 
   def current_user
-    @current_user ||= User.find_by(id: cookies[:user_id] if cookies[:user_id])
+    @current_user ||= User.find_by(id: cookies[:user_id]) if cookies[:user_id]
   end
 
   def current_shopping_cart
-    @current_shopping_cart ||= ShoppingCart.find_by(
-      id: current_user.current_card_id if current_user.current_card_id
-    )
+    @current_shopping_cart ||= ShoppingCart.find_by(id: current_user.current_card_id) if current_user.current_card_id
   end
 end
