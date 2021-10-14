0ba685f2e5c402856dd2b6e627826568970563b0
Chandra Lama <iamme24cl@gmail.com>
2021-10-14 10:02:54 -0700

    added column token to Users table sote users login token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_10_193039) do
+ActiveRecord::Schema.define(version: 2021_10_13_212801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_10_10_193039) do
     t.integer "current_card_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "token"
   end
 
   create_table "varietals", force: :cascade do |t|
